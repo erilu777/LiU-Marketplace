84ff3dada12b20c666066f9823c1a11771024816
frein784 <frein784@student.liu.se>
2024-03-07 11:56:50 +0100

    skapat class user

--- server/main.py
+++ server/main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
+from flask import abort
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return jsonify("Hello, World!")
+app = Flask(__name__, 
+static_folder='../client', 
+static_url_path='/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route("/")
+def client():
+  return app.send_static_file("client.html")
+
+
+
+class User:
+    name = db.Column(db.String(80), unique=True, nullable=False, primary_key=False)
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False, primary_key=False)
+    year = db.Column(db.Integer, unique=False, nullable=True, primary_key=False)
+    program = db.Column(db.String(80), unique=False, nullable=True, primary_key=False)
+        
 
 if __name__ == "__main__":
     app.run(port=5000)
