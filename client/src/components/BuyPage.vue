e3a5999e945bc783d3c044de1de8fda9db5e5abc
Ella Gripne <ellagripne@Ellas-MacBook-Air-2.local>
2024-04-04 17:29:37 +0200

    sökfunktionen fixad och exempelbilder tillagda

--- client/src/components/BuyPage.vue
+++ client/src/components/BuyPage.vue
@@ -3,8 +3,7 @@
     <h1>{{ pageTitle }}</h1>
     <!-- Search Bar -->
     <div class="search-bar">
-      <input type="text" v-model="searchTerm" placeholder="&#128269;" class="search-input">
-      <span @click="clearSearch">&#10006;</span>
+      <input type="search" v-model="searchTerm" placeholder="&#128269;" class="search-input">
     </div>
     
     <!-- Categories -->
@@ -25,15 +24,26 @@
         <option value="price">Lägst pris</option>
       </select>
     </div>
-    <Ads/>
-  </div>
+
+
+<div class="ads">
+  <router-link v-for="ad in filteredItems" :key="ad.id" :to="{ name: 'AdsDetails', params: { id: ad.id }, query: { imageUrl: ad.imageUrl, name: ad.name, price: ad.price, condition: ad.condition, location: ad.location, category: ad.category, description: ad.description }}" class="ad">
+    <img :src="ad.imageUrl" alt="Product Image" class="ad-image">
+    <div class="ad-details">
+    <h3 class="ad-title">{{ ad.name }}</h3>
+    <p class="ad-price">{{ ad.price }}</p>
+    </div>
+  </router-link>
+</div>
+</div>
 </template>
 
 <script>
-import Ads from '@/components/AdsSection.vue';
+import * as ads from '@/components/AdsItems.js';
+
 export default {
   components: {
-    Ads
+    
   },
   data() {
     return {
@@ -48,8 +58,19 @@ export default {
         { id: 6, name: 'Verktyg' },
         { id: 7, name: 'Övrigt' }
       ],
-      selectedCategories: []
+      selectedCategories: [],
+      filteredItems: [],
     };
+},
+watch: {
+  searchTerm: function () {
+    this.filteredItems = ads.adsData.filter((ad) =>
+      ad.category && ad.category.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  },
+},
+created() {
+  this.filteredItems = ads.adsData
 },
   methods: {
     toggleCategory(categoryId) {
@@ -136,5 +157,56 @@ export default {
   margin-left: 20px;
   padding: 5px;
 }
+
+.ads {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    max-width: 100%;
+    margin-left: 300px;
+    margin-right: 300px;
+    margin-top: 20px;
+  }
+  
+  .ad {
+    width: calc(33.33% - 20px); /* Three ads per row */
+    margin-bottom:  50px;
+    border: 2px solid #ccc;
+    border-radius: 15px;
+    overflow: hidden;
+    background-color: #E7F2F7;
+    border-color: #BBD5EA;
+    text-decoration: none;
+  }
+
+  p {
+  color: #102A50;
+  }
+  
+  .ad-image {
+    width: 250px;
+    height: auto;
+    padding: 10px;
+    align-items: center;
+    max-width: 100%;
+  }
+  
+  .ad-details {
+    padding: 10px;
+  }
+  
+  .ad-title {
+    margin-top: 0;
+    margin-bottom: 5px;
+    font-size: 15px;
+    color: #102A50;
+    font-weight: bold;
+    text-align: left;
+  }
+  
+  .ad-price {
+    margin: 0;
+    text-align: left;
+  }
 
 </style>
