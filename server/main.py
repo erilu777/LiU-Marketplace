7020b6bc60025a16f9f9c1a0483174eee06620d7
Axel Stridh <axest556@student.liu.se>
2024-04-08 10:01:48 +0200

    Authentication fixat

--- server/main.py
+++ server/main.py
@@ -1,16 +1,27 @@
 from flask import Flask, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
 from sqlalchemy.orm import relationship
 from enum import Enum
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 
 app = Flask(__name__, 
 static_folder='../client', 
 static_url_path='/')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your_very_complex_string_here'
 db = SQLAlchemy(app)
-##jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+
+@app.route("/")
+def client():
+  return app.send_static_file("client.html")
 
 class User(db.Model):
 
@@ -26,6 +37,7 @@ class User(db.Model):
     #items = db.relationship('Item', backref="user", foreign_keys="Item.user_id")
     sold_items = db.relationship('Item', backref="seller", foreign_keys="Item.seller_id")
     bought_items = db.relationship('Item', backref="buyer", foreign_keys="Item.buyer_id")
+    password_hash = db.Column(db.String, nullable=False)
 
     @property
     def email(self):
@@ -46,6 +58,10 @@ class User(db.Model):
             "num_sold_items":self.num_sold_items,
             "num_bought_items":self.num_bought_items
         } 
+    
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf8')
+
         
 class Category(Enum):
     Cyklar = 1
@@ -119,11 +135,9 @@ class Item(db.Model):
             "buyer": buyer_data
         }
 
-@app.route("/")
-def client():
-  return app.send_static_file("client.html")
 
 @app.route('/users', methods=['GET', 'POST'])
+@jwt_required()
 def users():
     if request.method == 'GET':
         all_users = User.query.all()
@@ -136,6 +150,7 @@ def users():
         return jsonify(new_user.serialize()), 201
     
 @app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+@jwt_required()
 def handle_users(user_id):
     user = User.query.get(user_id)
     if user is None:
@@ -160,6 +175,7 @@ def handle_users(user_id):
         return '', 200
     
 @app.route('/items', methods=['GET', 'POST'])
+@jwt_required()
 def items():
     if request.method == 'GET':
         all_items = Item.query.all()
@@ -176,6 +192,7 @@ def items():
         return jsonify(new_item.serialize()), 201
     
 @app.route('/items/<int:item_id>', methods=['GET', 'PUT', 'DELETE'])
+@jwt_required()
 def handle_items(item_id):
     item = Item.query.get(item_id)
     if item is None:
@@ -204,6 +221,7 @@ def handle_items(item_id):
         return '', 200
 
 @app.route('/items/<int:item_id>/sell', methods=['PUT'])
+@jwt_required()
 def sell_item(item_id):
     item = Item.query.get(item_id)
     if item is None:
@@ -231,6 +249,32 @@ def sell_item(item_id):
     # TODO: Send an email to the buyer
 
     return jsonify(item.serialize()), 200
+
+
+@app.route('/sign-up', methods=['POST'])
+def signup():
+    data = request.get_json()
+    new_user = User(liu_id=data['liu_id'])
+    password = data['password']
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'}), 201
+
+
+@app.route('/login', methods = ['POST'])
+def login():
+    data = request.get_json()
+    if not data or not data.get('liu_id') or not data.get('password'):
+        return jsonify({'msg': 'Missing liu_id or password'}), 400
+    
+    user = User.query.filter_by(liu_id=data['liu_id']).first()
+
+    if user and bcrypt.check_password_hash(user.password_hash, data['password']):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"token": access_token, "user": user.serialize()}), 200    
+    else:
+        return jsonify({'msg': 'Invalid email or password'}), 401
 
 
 if __name__ == "__main__":
