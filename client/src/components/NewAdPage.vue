e39cf814300de2f541eb28ddd111ecbb0aab330f
frein784 <frein784@student.liu.se>
2024-04-10 11:12:24 +0200

    nu funkar det här

--- client/src/components/NewAdPage.vue
+++ client/src/components/NewAdPage.vue
@@ -1,46 +1,46 @@
 <template>
-    <div class="page">
-      <h1>Försäljning av {{ category }}</h1>
-      <div class="container">
-        <form @submit.prevent="addItem">
-            <div class="row">
-                <input type="text" id="title" v-model="title" placeholder="Titel" class="ctrph">
-            </div>
-            <div class="row">
-                <textarea id="description" v-model="description" placeholder="Beskrivning" class="ctrph"></textarea>
-            </div>
-            <div class="row">
-                <input type="number" id="price" v-model="price" placeholder="Pris" class="ctrph">
-            </div>
-            <div class="row">
-                <select id="area" v-model="area">
-                    <option value="" disabled selected>Område</option>
-                    <option value="linkoping">Linköping</option>
-                    <option value="norrkoping">Norrköping</option>
-                </select>
-            </div>
-            <div class="row">
-                <select id="condition" v-model="condition">
-                    <option value="" disabled selected>Skick</option>
-                    <option value="Nytt">Nytt</option>
-                    <option value="Använd_Nyskick">Använd - nyskick</option>
-                    <option value="Använd_Gott_skick">Använd - gott skick</option>
-                    <option value="Använd_Slitet_skick">Använd - använt skick</option>
-                </select>
-            </div>
-            <div class="row">
-                <input type="file" id="image" accept="image/*" @change="handleImageUpload">
-            </div>
-            <!--<button type="submit">Gå till betalning</button>-->
-            <button type="submit" @click="navigateToPay">Gå till betalning</button>-->
-        </form>
-      </div>
+  <div class="page">
+    <h1>Försäljning av {{ category }}</h1>
+    <div class="container">
+      <form @submit.prevent="addItem">
+        <div class="row">
+          <input type="text" id="title" v-model="title" placeholder="Titel" class="ctrph">
+        </div>
+        <div class="row">
+          <textarea id="description" v-model="description" placeholder="Beskrivning" class="ctrph"></textarea>
+        </div>
+        <div class="row">
+          <input type="number" id="price" v-model="price" placeholder="Pris" class="ctrph">
+        </div>
+        <div class="row">
+          <select id="area" v-model="area">
+            <option value="" disabled selected>Område</option>
+            <option value="Linköping">Linköping</option>
+            <option value="Norrköping">Norrköping</option>
+          </select>
+        </div>
+        <div class="row">
+          <select id="condition" v-model="condition">
+            <option value="" disabled selected>Skick</option>
+            <option value="Nytt">Nytt</option>
+            <option value="Använd_Nyskick">Använd - nyskick</option>
+            <option value="Använd_Gott_skick">Använd - gott skick</option>
+            <option value="Använd_Slitet_skick">Använd - använt skick</option>
+          </select>
+        </div>
+        <div class="row">
+          <input type="file" id="image" accept="image/*" @change="handleImageUpload">
+        </div>
+        <button type="submit">Gå till betalning</button>
+        <!--<button type="submit" @click="navigateToPay">Gå till betalning</button>-->
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
-  export default {
+export default {
   data() {
     return {
       category: '',
@@ -52,32 +52,34 @@ import axios from 'axios';
     this.category = this.$route.params.category;
   },
   methods: {
-    
+
     addItem() {
-  const token = JSON.parse(sessionStorage.getItem('auth')).token; // Hämta token från sessionStorage
-  console.log('Token:', token); // Log the value of the token
+      const token = JSON.parse(sessionStorage.getItem('auth')).token; // Hämta token från sessionStorage
+      console.log('Token:', token); // Log the value of the token
 
-  const data = {
-    "category": this.category,
-    "title": this.title,
-    "description": this.description,
-    "price": this.price,
-    "condition": this.condition,
-    //"seller_id": 1
-  };
+      const data = {
+        "category": this.category,
+        "title": this.title,
+        "description": this.description,
+        "price": this.price,
+        "condition": this.condition,
+        "area": this.area
+        //"seller_id": 1
+      };
 
-  axios.post('/items', data, {
-    headers: {
-        "Authorization": "Bearer " + token
-      }
-  })
-  .then(response => {
-    console.log('Response:', response);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
-},
+      axios.post('/items', data, {
+        headers: {
+          "Authorization": "Bearer " + token
+        }
+      })
+        .then(response => {
+          console.log('Response:', response);
+        })
+        .catch(error => {
+          console.log('Error:', error);
+        });
+        this.$router.push('/payment');
+    },
     handleImageUpload(event) {
       const file = event.target.files[0];
       this.image = file;
@@ -91,50 +93,50 @@ import axios from 'axios';
     }
 
   }
-  };
-  </script>
+};
+</script>
 
 <style scoped>
 .page {
-    padding: 60px;
-    min-height: 80vh;
-    margin: 0 auto;
-  }
+  padding: 60px;
+  min-height: 80vh;
+  margin: 0 auto;
+}
 
-  h1 {
-    font-weight: bold;
-    font-size: 60px;
-    color: #0c264d;
-    padding-bottom: 40px;
-  }
+h1 {
+  font-weight: bold;
+  font-size: 60px;
+  color: #0c264d;
+  padding-bottom: 40px;
+}
 
-  .container {
-    background-color: #e7f2f7;
-    border: 3px solid #bbd5e9;
-    border-radius: 20px;
-    width: 700px;
-    margin: 0 auto;
-    padding: 50px;
-  }
+.container {
+  background-color: #e7f2f7;
+  border: 3px solid #bbd5e9;
+  border-radius: 20px;
+  width: 700px;
+  margin: 0 auto;
+  padding: 50px;
+}
 
-  .row {
-    padding: 10px;
-    width: 70%;
-    margin: 0 auto;
-  }
+.row {
+  padding: 10px;
+  width: 70%;
+  margin: 0 auto;
+}
 
-  button {
-    background-color: #0c264d;
-    border-color: #bbd5e9;
-    padding: 10px;
-    width: 40%;
-    border-radius: 20px;
-    color: white;
-    font-weight: bold;
-    margin-top: 15px;
-  }
+button {
+  background-color: #0c264d;
+  border-color: #bbd5e9;
+  padding: 10px;
+  width: 40%;
+  border-radius: 20px;
+  color: white;
+  font-weight: bold;
+  margin-top: 15px;
+}
 
-  .ctrph::placeholder {
-    text-align: center; 
-  }
+.ctrph::placeholder {
+  text-align: center;
+}
 </style>
