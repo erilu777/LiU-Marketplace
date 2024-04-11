500f3168110dcf153a9c1ed5cef0df965f6d14c5
Erik Andersson <erian766@student.liu.se>
2024-04-11 15:40:51 +0200

    nu är buyPage fixad

--- client/src/components/AdsItems.js
+++ client/src/components/AdsItems.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export async function fetchAdsData() {
   try {
-    const token = JSON.parse(sessionStorage.getItem('auth')).token; // Hämta token från sessionStorage
+    const token = JSON.parse(sessionStorage.getItem('auth')).token; // Fetch token from sessionStorage
     console.log('Token:', token); // Log the value of the token
 
-    const response = await axios.get('/items', {
+    const response = await axios.get('/all_available_items', {
       headers: {
         "Authorization": "Bearer " + token
       }
