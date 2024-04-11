d8163e678d7da2e3128cdd9b41e8a292aab94282
frein784 <frein784@student.liu.se>
2024-04-11 15:02:35 +0200

    visar nu namn om det finns

--- client/src/components/ProfilePage.vue
+++ client/src/components/ProfilePage.vue
@@ -1,123 +1,122 @@
 <template>
-    <div class="page">
+  <div class="page">
+    <div class="row">
+      <div class="profile-container">
         <div class="row">
-            <div class="profile-container">
-                <div class="row">
-                    <img src="../assets/profile.png" alt="LMlogo">
-                </div>
-                <div class="row">
-                    <h2>{{ user.firstName }} {{ user.lastName}}</h2>
-                </div>
-                <div class="row">
-                    <h4>{{ user.email }}</h4>
-                </div>
-                <div class="row">
-                    <h4>{{ user.education }}</h4>
-                </div>
-                <div class="row">
-                    <h4>Årskurs {{ user.year }}</h4>
-                </div>
-                <div class="row">
-                  <button class="edit-button" @click="navigateToEdit">Redigera profil</button>
-                </div>
-                <div class="row">
-                  <button class="h-button" @click="navigateToHistory">Visa nuvarande annonser och historik</button>
-                </div>
-            </div>
-          </div>  
+          <img src="../assets/profile.png" alt="LMlogo">
+        </div>
+        <div class="row">
+          <h1 v-if="user.name">{{ user.name }}</h1>
+        </div>
+        <div class="row">
+          <h4>{{ user.email }}</h4>
+        </div>
+        <div class="row">
+          <h4>{{ user.education }}</h4>
+        </div>
+        <div class="row">
+          <h4>Årskurs {{ user.year }}</h4>
+        </div>
+        <div class="row">
+          <button class="edit-button" @click="navigateToEdit">Redigera profil</button>
+        </div>
+        <div class="row">
+          <button class="h-button" @click="navigateToHistory">Visa nuvarande annonser och historik</button>
+        </div>
       </div>
+    </div>
+  </div>
 </template>
-  
-  <script>
-  import axios from 'axios';
 
-  export default {
-    data() {
-      return {
-        user: {
-          firstName: '',
-          lastName: '',
-          email: '',
-          education: '',
-          year: '',
-        },
-      };
-    },
+<script>
+import axios from 'axios';
 
-    async created() {
-      const token = JSON.parse(sessionStorage.getItem('auth')).token;
-      const response = await axios.get('/current_user', {
-        headers: {
-          "Authorization": "Bearer " + token
-        },
-      });
-      this.user = response.data;
-    },
-
-
-    methods: {
-      navigateToEdit() {
-        this.$router.push('/edit-profile');
+export default {
+  data() {
+    return {
+      user: {
+        real_name: JSON.parse(sessionStorage.getItem('auth')).user.name,
+        firstName: '',
+        lastName: '',
+        email: '',
+        education: '',
+        year: '',
       },
-      navigateToHistory() {
-        this.$router.push('/profile-history');
-      }
+    };
+  },
+
+  async created() {
+    const token = JSON.parse(sessionStorage.getItem('auth')).token;
+    const response = await axios.get('/current_user', {
+      headers: {
+        "Authorization": "Bearer " + token
+      },
+    });
+    this.user = response.data;
+  },
+
+
+  methods: {
+    navigateToEdit() {
+      this.$router.push('/edit-profile');
+    },
+    navigateToHistory() {
+      this.$router.push('/profile-history');
     }
-  };
-  </script>
-  
-  <style scoped>
-  .page {
-    min-height: 80vh;
-    margin: 0 auto;
   }
+};
+</script>
 
-  h2 {
-    font-weight: bold;
-    color: black;
-  }
+<style scoped>
+.page {
+  min-height: 80vh;
+  margin: 0 auto;
+}
 
-  h4 {
-    color: black;
-  }
+h2 {
+  font-weight: bold;
+  color: black;
+}
 
-  img {
-    max-width: 300px;
-    margin: 0 auto;
-  }
+h4 {
+  color: black;
+}
 
-  .profile-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+img {
+  max-width: 300px;
+  margin: 0 auto;
+}
 
-  .row {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 5px;
-  }
+.profile-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .edit-button {
-    display: flex;
-    align-items: center;
-    background-color: #0c264d;
-    padding: 10px;
-    height: 40px;
-    color: white;
-    margin-bottom: 7px;
-    margin-top: 7px;
-    text-decoration: none;
-  }
+.row {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+}
 
-  .h-button {
-    display: flex;
-    align-items: center;
-    padding: 10px;
-    height: 40px;
-    color: #0c264d;
-    text-decoration: none;
-  }
-  
-  </style>
-  
+.edit-button {
+  display: flex;
+  align-items: center;
+  background-color: #0c264d;
+  padding: 10px;
+  height: 40px;
+  color: white;
+  margin-bottom: 7px;
+  margin-top: 7px;
+  text-decoration: none;
+}
+
+.h-button {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  height: 40px;
+  color: #0c264d;
+  text-decoration: none;
+}
+</style>
